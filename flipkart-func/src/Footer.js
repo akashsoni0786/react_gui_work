12437f64f670a1344df19d238855df5aa700050a
akashsoni0786 <akashsoni@cedcoss.com>
2022-07-12 18:57:17 +0530

    Done

---
+++ flipkart-func/src/Footer.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import "./Footer.css";
+const Footer = () => {
+  return (
+     <div className='d-flex flex-row '> 
+  <Products />
+         <div style={{float:"right"}}><img   type="button" width='100%' alt='' src='https://rukminim1.flixcart.com/flap/464/708/image/a60eb148185597ad.jpg?q=70' /></div>
+       </div>
+  )
+}
+
+
+
+export const Products = () => {
+  return (
+    
+
+<div className="row mb-5 ">
+      <div className="col-12">
+        <div id="carouselExampleIndicators2" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+            <div className="row">
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}}  alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                      <p id="cardstyle"  className=" text-success">Up to 70% Off</p>
+                      <p type="button" id="cardstyle">Best Discount &nbsp;</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}}  alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                      <p id="cardstyle"className="text-success ">Up to 70% Off</p>
+                      <p type="button" id="cardstyle">Best Discount</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}} alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                      <p id="cardstyle"className="text-success ">Up to 70% Off</p>
+                      <p type="button" id="cardstyle">Best Discount</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}} alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                      <p id="cardstyle"className="text-success">Up to 70% Off</p>
+                      <p type="button" id="cardstyle">Best Discount</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}}  alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                    <p id="cardstyle"className="text-success">Up to 70% Off</p>
+                    <p type="button" id="cardstyle">Best Discount</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}}  alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                      <p id="cardstyle"  className=" text-success">Up to 70% Off</p>
+                      <p type="button" id="cardstyle">Best Discount &nbsp;</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}}  alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                      <p id="cardstyle"className="text-success ">Up to 70% Off</p>
+                      <p type="button" id="cardstyle">Best Discount</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}} alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                      <p id="cardstyle"className="text-success ">Up to 70% Off</p>
+                      <p type="button" id="cardstyle">Best Discount</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}} alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                      <p id="cardstyle"className="text-success">Up to 70% Off</p>
+                      <p type="button" id="cardstyle">Best Discount</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-3 mb-3">
+                  <div className="card">
+                    <div className="img-wrapper">
+                    <img type="button" src="https://rukminim1.flixcart.com/image/150/150/krntoy80/cookware-set/g/n/x/kan028-kreme-original-imag5e7extvftecn.jpeg?q=70" style={{width:"100%"}}  alt="..."/>
+                    </div>
+                    <div className="card-body">
+                    <p type="button"  style={{fontSize:".8rem",fontWeight:"bold",marginBottom:"-4px"}} >Kitchen Dinning Set</p>
+                    <p id="cardstyle"className="text-success">Up to 70% Off</p>
+                    <p type="button" id="cardstyle">Best Discount</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <button className="carousel-control-prev text-light" type="button" data-bs-target="#carouselExampleIndicators2"
+            data-bs-slide="prev">
+            <span className="carousel-control-prev-icon text-light" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next text-light" type="button" data-bs-target="#carouselExampleIndicators2"
+            data-bs-slide="next">
+            <span className="carousel-control-next-icon text-light" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+
+export default Footer
---
+++ flipkart-func/src/HeaderOptions.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './App.css';
+const HeaderOptions =()=>{
+ 
+  return (
+    <div className='hearderOptionBg'>
+
+        <div><img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100'/><p style={{fontSize:".7rem"}} type="button">Top Offers</p></div>
+   
+
+        <div><img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100'/><p style={{fontSize:".7rem",}}  type="button"><select className='optionsHeader'>
+          <option selected disabled>Grocrey</option>
+          <option>Op1</option>
+          </select></p></div>
+
+    <div><img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100'/><p type="button" style={{fontSize:".7rem"}}><select className='optionsHeader'>
+          <option selected disabled>Mobiles</option>
+          <option>Op1</option>
+          </select></p></div>
+    <div>
+    <img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/c12afc017e6f24cb.png?q=100'/><p type="button" style={{fontSize:".7rem"}}><select className='optionsHeader'>
+          <option selected disabled>Fashion</option>
+          <option>Op1</option>
+          </select></p></div>
+    <div>
+    <img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100'/><p type="button" style={{fontSize:".7rem"}}><select className='optionsHeader'>
+          <option selected disabled>Electronics</option>
+          <option>Op1</option>
+          </select></p></div>
+    <div>
+    <img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100'/><p type="button" style={{fontSize:".7rem"}}><select className='optionsHeader'>
+          <option selected disabled>Home</option>
+          <option>Op1</option>
+          </select></p></div>
+    <div>
+    <img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100'/><p type="button" style={{fontSize:".7rem"}}><select className='optionsHeader'>
+          <option selected disabled>Appliance</option>
+          <option>Op1</option>
+          </select></p></div>
+    <div>
+    <img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100'/><p type="button" style={{fontSize:".7rem"}}><select className='optionsHeader'>
+          <option selected disabled>Travel</option>
+          <option>Op1</option>
+          </select></p></div>
+    <div>
+    <img type="button" className='headeroptionlogos'  alt='' src='https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100'/><p type="button" style={{fontSize:".7rem"}}>Beauty,Toy & More</p></div></div>
+  )
+}
+
+export default HeaderOptions
